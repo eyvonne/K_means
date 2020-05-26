8094a85025cd7bd5cb9a6b79f7cd89e42d96300f
eyvonne <eyvonne@users.noreply.github.com>
2020-05-25 19:25:16 -0700

    not sure

--- src/k_nearest_neighbors.py
+++ src/k_nearest_neighbors.py
@@ -15,8 +15,4 @@ class KNearestNeighbors():
         return self.data[kmin_index[:self.neighbors]], dist[kmin_index[:self.neighbors]]
 
 
-if __name__ == '__main__':
-    arr = [2, 4, 5, 6, 3, 7, 8, 9, 1, 0, 23, 43, 32, 23, 12, 21, 56, 76, 87, 54, 34, 78, 67, 65]
-    neigh = KNearestNeighbors(2)
-    neigh.fit(arr)
-    print(neigh.predict(60))
+

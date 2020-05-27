8132d9e87f2b334b75903aa53fe8beec8e7f2de7
eyvonne <eyvonne@users.noreply.github.com>
2020-05-27 14:55:31 -0700

    implemented as class method

--- src/k_nearest_neighbors.py
+++ src/k_nearest_neighbors.py
@@ -18,3 +18,30 @@ class KNearestNeighbors():
         dist = spatial.distance_matrix(self.data, y)
         kmin_index = np.argsort(dist, axis=0)
         return self.data[kmin_index[:self.neighbors]], dist[kmin_index[:self.neighbors]]
+
+    
+class QuakeNearestNeighbors():
+    def __init__(self, neighbors):
+        self.neighbors = neighbors
+
+    def fit(self, data):
+        self.data = data
+        self.matching = data[['lat', 'lon', 'mag']].values
+
+    def predict(self, y):
+        match = y[['lat','lon','mag']].values
+        dist = spatial.distance_matrix(self.matching, [match])
+        kmin_index = np.argsort(dist, axis=0)
+        return kmin_index[:self.neighbors], dist[kmin_index[:self.neighbors]]
+    
+    def pair_quakes(self, df):
+        '''this is a reusable function after fit has been run. It takes in 
+        a dataframe and then returns a list of tuples with ID's of matching 
+        quakes''' 
+        preds = []
+        for i, row in df.iterrows():
+            neighbor_index, dist = near.predict(row)
+            pred = emsc_df.iloc[neighbor_index.squeeze()]
+            if dist < 1:
+                preds.append((pred['id'], row['id']))
+        return preds
